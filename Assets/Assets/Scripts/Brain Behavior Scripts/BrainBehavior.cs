259a6469740df2ce4bc8e7a0663dafb41dce826f
Walt Destler <walt@waltdestler.com>
2016-01-30 15:08:33 -0800

    Added separate brain animation which is now colored depending on the brain behavior.

--- Assets/Assets/Scripts/Brain Behavior Scripts/BrainBehavior.cs
+++ Assets/Assets/Scripts/Brain Behavior Scripts/BrainBehavior.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BrainBehavior : MonoBehaviour {
 
 	public CharacterBody body;
+	public Color brainColor = Color.magenta;
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Assets/Scripts/Brain Behavior Scripts/BrainSpriteController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class BrainSpriteController : MonoBehaviour
+{
+	public CharacterBody Body;
+	public SpriteRenderer BrainSprite;
+
+	public void Update()
+	{
+		if (Body != null && BrainSprite != null)
+		{
+			BrainSprite.enabled = Body.brainBehavior != null;
+			if (Body.brainBehavior != null)
+				BrainSprite.color = Body.brainBehavior.brainColor;
+		}
+	}
+}
