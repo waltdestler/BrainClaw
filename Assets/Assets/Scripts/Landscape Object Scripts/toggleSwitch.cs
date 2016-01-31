4d105e09b59344e1f2b8b4fa277917236fedcf8f
Stephanie H. Nguyen <byronic.string@gmail.com>
2016-01-30 16:46:16 -0800

    Added/amended moveLeft, toggleSwitch, switchTest.
    Created moveLeft script, added connected door to toggleSwitch script,
    reorganized some files in file script, and saved switchTest.

---
+++ Assets/Assets/Scripts/Landscape Object Scripts/toggleSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toggleSwitch : MonoBehaviour {
+
+	public GameObject doorConnected;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnTriggerEnter2D (Collider2D player) {
+		if (player.tag == "character") {
+			// Flip object asset along x-axis. 
+			var scale = gameObject.transform.localScale;
+			scale.x *= -1;
+			gameObject.transform.localScale = scale;
+			// Open lift/open door... For now, just hide and show.
+			doorConnected.SetActive (!doorConnected.activeSelf);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
