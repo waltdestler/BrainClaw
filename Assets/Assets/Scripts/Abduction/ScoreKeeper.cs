b48e3e0d0b8c07e4e21c47335b1b9f0f333fdd28
Stephanie H. Nguyen <byronic.string@gmail.com>
2016-01-31 13:45:01 -0800

    Stylized scoreKeeper.
    Font, size, color.

--- Assets/Assets/Scripts/Abduction/ScoreKeeper.cs
+++ Assets/Assets/Scripts/Abduction/ScoreKeeper.cs
@@ -5,16 +5,20 @@ using System.Collections;
 public class ScoreKeeper : MonoBehaviour {
 	public int numLeftToAbduct = 3;
     public bool showText = true;
-    public Rect textArea = new Rect(0,0,Screen.width, Screen.height);
+	private Rect textArea;
+	private GUIStyle style = new GUIStyle();
 
 	// Use this for initialization
 	void Start () {
-        
+		textArea = new Rect(0,0, Screen.width, Screen.height);
+		style.fontSize = 20;
+		style.normal.textColor = Color.cyan;
+		style.font = (Font)Resources.Load("slkscr");
 	}
-		
+
     void OnGUI() {
 		var textDisplay = "Abduction Quota: " + numLeftToAbduct;
-        GUI.Label(textArea,textDisplay);
+        GUI.Label(textArea,textDisplay, style);
     }
 	
 	// Update is called once per frame
