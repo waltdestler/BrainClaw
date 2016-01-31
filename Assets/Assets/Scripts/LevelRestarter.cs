e9bdc5fcfbf5a7c091cf0e019e6a8aba4166622d
Walt Destler <walt@waltdestler.com>
2016-01-31 11:12:43 -0800

    Pressing R will now restart the level.

---
+++ Assets/Assets/Scripts/LevelRestarter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestarter : MonoBehaviour
+{
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
