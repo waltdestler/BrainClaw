b7133b8b653d13061d486c40ab2592d549b8f81b
Stephanie H. Nguyen <byronic.string@gmail.com>
2016-01-31 14:55:30 -0800

    configurable win condition text color

--- Assets/Assets/Scripts/Abduction/ScoreKeeper.cs
+++ Assets/Assets/Scripts/Abduction/ScoreKeeper.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class ScoreKeeper : MonoBehaviour {
 	public int numLeftToAbduct = 3;
     public bool showText = true;
+	public Color textColor = Color.cyan;
 	private Rect textArea;
 	private GUIStyle style = new GUIStyle();
 
@@ -12,7 +13,7 @@ public class ScoreKeeper : MonoBehaviour {
 	void Start () {
 		textArea = new Rect(0,0, Screen.width, Screen.height);
 		style.fontSize = 20;
-		style.normal.textColor = Color.cyan;
+		style.normal.textColor = textColor;
 		style.font = (Font)Resources.Load("slkscr");
 	}
 
