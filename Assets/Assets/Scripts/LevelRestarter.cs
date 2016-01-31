c64a9e69bbc0cfbe72e75a81153f99dba9de7abb
Albert Shih <albertbshih@gmail.com>
2016-01-31 15:28:53 -0800

    Added stages

--- Assets/Assets/Scripts/LevelRestarter.cs
+++ Assets/Assets/Scripts/LevelRestarter.cs
@@ -7,5 +7,13 @@ public class LevelRestarter : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.R))
 			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
+		if (Input.GetKeyDown (KeyCode.M)) {
+			Application.LoadLevel (Application.loadedLevel+1);
+		}
+		if (Input.GetKeyDown (KeyCode.N)) {
+			Application.LoadLevel (Application.loadedLevel-1);
+		}
 	}
 }
