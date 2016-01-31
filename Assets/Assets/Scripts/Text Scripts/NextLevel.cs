d48726d82c37660f0b71d2b1faa1c7de1f8e5252
Albert Shih <albertbshih@gmail.com>
2016-01-31 15:14:38 -0800

    New start screen

---
+++ Assets/Assets/Scripts/Text Scripts/NextLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoNextLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
