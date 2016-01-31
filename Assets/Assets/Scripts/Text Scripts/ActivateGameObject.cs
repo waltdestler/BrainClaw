7a940b4e7c01f5e898b1a881d490daab28992816
Albert Shih <albertbshih@gmail.com>
2016-01-31 14:06:41 -0800

    Added Door System prefab, fixed tiles for 4 levels, added text

---
+++ Assets/Assets/Scripts/Text Scripts/ActivateGameObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateGameObject : MonoBehaviour {
+
+	public void Deactivate()
+	{
+		gameObject.SetActive (false);
+	}
+
+	public void Activate()
+	{
+		gameObject.SetActive (true);
+	}
+}
---
+++ Assets/Assets/Scripts/Text Scripts/TriggerColliderEventSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class TriggerColliderEventSystem : MonoBehaviour {
+
+	public UnityEvent tiggerEvent;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject == target) {
+			tiggerEvent.Invoke ();
+		}
+	}
+}
+
