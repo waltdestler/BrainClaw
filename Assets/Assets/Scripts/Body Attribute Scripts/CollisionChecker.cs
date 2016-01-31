17a311866a0681c16839ad4236593cb4616b46a6
Jimmy <zaphos@gmail.com>
2016-01-30 22:04:35 -0800

    fix collision checker

--- Assets/Assets/Scripts/Body Attribute Scripts/CollisionChecker.cs
+++ Assets/Assets/Scripts/Body Attribute Scripts/CollisionChecker.cs
@@ -3,18 +3,11 @@ using System.Collections;
 
 public class CollisionChecker : MonoBehaviour {
 
-	public bool isColliding = false;
-
-
-	void OnTriggerStay2D(Collider2D c)
-	{
-		isColliding = true;
-	}
-
-
-	void OnTriggerExit2D(Collider2D c)
-	{
-		isColliding = false;
-	}
+	public bool isColliding 
+    {
+        get {
+            return gameObject.GetComponent<Collider2D>().IsTouchingLayers();
+        }
+    }
 
 }
